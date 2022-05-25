7c2f102be4d9168c2e905ad4286cd05a97a5d635
Adam Hamsik <adam.hamsik@lablabs.io>
2022-05-25 12:02:25 +0200

    Initial work on semver parsing github action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as semver from 'semver'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const tag: string = core.getInput('tag')
+    core.debug(`Received ${tag} for parsing`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const ut: string = tag.split('_')[0]
+    core.debug(`Upstream tag: ${ut}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.setOutput('time', new Date().toTimeString())
+    const sm: string = semver.parse(ut, { loose: true })
+
+    const mmp: string = `${sm.major}.${sm.minor}.${sm.patch}`
+    core.debug(`Upstream tag: ${mmp}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+
+    const mmp: string = `${sm.major}.${sm.minor}`
+    core.debug(`Upstream tag: ${mm}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+
+    core.setOutput('pft', tag)
+    core.setOutput('ut', ut)
+    core.setOutput('mmp', mmp)
+    core.setOutput('mm', mm)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
