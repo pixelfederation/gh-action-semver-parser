0ee2eb109bfc143117a288a957ea9f85dcbc2022
Adam Hamsik <adam.hamsik@lablabs.io>
2022-05-26 00:41:23 +0200

    Fix typescript build

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import * as semver from '@semver'
+import * as semver from 'semver'
 
 
 async function run(): Promise<void> {
@@ -12,19 +12,23 @@ async function run(): Promise<void> {
     core.debug(`Upstream tag: ${ut}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     // Parse exiting tag to SemVer object
-    const sm: SemVer = semver.parse(ut, { loose: true })
-    const release: string = sm.prerelease().split('-')[1]
+    const sm = semver.parse(ut, { loose: true })
+    if (!sm) {
+      core.setFailed(`Could not parse tag: ${ut}`);
+    }
+
+    const release: string = String(sm!.prerelease).split('-')[1]
 
     core.setOutput('pft', tag)
     core.setOutput('ut', ut)
 
     // Create output for 8.1.3 and 8.1
-    core.setOutput('mmp', `${ sm.major }.${ sm.minor }.${ sm.patch }`)
-    core.setOutput('mm',  `${ sm.major }.${ sm.minor }`)
+    core.setOutput('mmp', `${ sm!.major }.${ sm!.minor }.${ sm!.patch }`)
+    core.setOutput('mm',  `${ sm!.major }.${ sm!.minor }`)
 
     // Create tags 8.1.3-bullseye and 8.1-bullseye
-    core.setOutput('mmpr', `${ sm.major }.${ sm.minor }.${ sm.patch }-${ release }`)
-    core.setOutput('mmr',  `${ sm.major }.${ sm.minor }-${ release }`)
+    core.setOutput('mmpr', `${ sm!.major }.${ sm!.minor }.${ sm!.patch }-${ release }`)
+    core.setOutput('mmr',  `${ sm!.major }.${ sm!.minor }-${ release }`)
 
     // Output image debian release
     core.setOutput('release', release)
